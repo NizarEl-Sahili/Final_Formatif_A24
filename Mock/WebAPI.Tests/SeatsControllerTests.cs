07e98ef428bfa467cffb76e2bcad9bb281cf2c5b
2393333 <2393333@cegepmontpetit.ca>
2025-05-21 12:55:49 -0400

    Progres: Mock

--- Mock/WebAPI.Tests/SeatsControllerTests.cs
+++ Mock/WebAPI.Tests/SeatsControllerTests.cs
@@ -1,14 +1,29 @@
 ﻿using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebAPI.Controllers;
+using WebAPI.Services;
 
 namespace WebAPI.Tests;
 
 [TestClass]
 public class SeatsControllerTests
 {
+    Mock<SeatsService> mock1;
+    Mock<SeatsController> mock2;
+    public SeatsControllerTests()
+    {
+        mock1 = new Mock<SeatsService>();
+        mock2 = new Mock<SeatsController>(mock1.Object) { CallBase = true };
+        mock2.Setup(c => c.UserId).Returns("11111");
+    }
+
     [TestMethod]
     public void ReserveSeat()
     {
-        
+
+
+
+
     }
 }
